ecb66ab8b18a6500fcaea73413a2938b2d538399
lidia <lidiabara7@gmail.com>
2019-07-28 21:45:44 -0700

    moved the file views outside of the file controllers

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,18 +1,91 @@
 var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  //the selectAll is refering to the object in the orm and the model
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new row
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
+
+
+
+
+
+var express = require("express");
 var burgerFile = require("../models/burger.js");
 var router = express.Router();
 //Create the `router` for the app, and export the `router` at the end of your file.
 
 //to redirect to the home page
 router.get("/", function(req,res){
-    res.redirect("/index");
+    burger.all(function(data) {
+        var hbsObject = {
+          burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+      });
 });
 
 //this to display all the burgers
-router.get("/index", function (req, res){
+// router.get("/index", function (req, res){
 // burgerFile.selectAll(function(data){
     // var burgerObject = {burgers:data};
     // res.render(//idk)
 // })
-})
+// })
 
